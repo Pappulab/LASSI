af1fd7ba51ed1df91b352cb3bfe582312b6eacd0
FurqanDar <darf@kenyon.edu>
2019-10-12 10:22:47 -0500

    Adding optimization flags to compilation

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-gcc -Og -g -pg main.c parsekey.c print.c initialize.c structure.c energy.c cluster.c mcmove.c -o main -lm
+gcc -g -pg main.c parsekey.c print.c initialize.c structure.c energy.c cluster.c mcmove.c -o main -lm
 cp -u main /work/fdar/LatticeModels/LASSI_GIT/TESTS/GDBTest/
 gcc -O3 main.c parsekey.c print.c initialize.c structure.c energy.c cluster.c mcmove.c -o main -lm
 cp -u main runs/
