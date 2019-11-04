eeaa6246a37a0c126ee315073669f681499150e4
FurqanDar <darf@kenyon.edu>
2019-11-04 16:47:01 -0600

    Additional commenting.

--- main.c
+++ main.c
@@ -8,19 +8,22 @@
 #include "cluster.h"
 
 int main(int argc, char *argv[]) {
+    //Read in the system commands
     char keyfile[100];
-    char lBrace[] = "<======      ";
-    char rBrace[] = "      ======>";
     if (argc == 3 && strcmp(argv[1], "-k") == 0) {
         strcpy(keyfile, argv[2]);
     } else {
         strcpy(keyfile, "param.key");
     }
-
+    //Text formatting helpers.
+    char lBrace[] = "<======      ";
+    char rBrace[] = "      ======>";
+    //Read the param file!
     int errorcode;
     errorcode = Parse_Keyfile(keyfile);
     if (errorcode == 0) {
         printf("Key file %s was successfully parsed.\n\n", keyfile);
+        srand(RNG_Seed == 0 ? time(NULL) : RNG_Seed);
         Print_Key();
     } else {
         printf("ERROR: unable to parse key file %s.\n", keyfile);
@@ -29,19 +32,18 @@ int main(int argc, char *argv[]) {
 
     printf("%s Initializing %s\n", lBrace, rBrace);
 
-    srand(RNG_Seed == 0 ? time(NULL) : RNG_Seed);
-
-
     clock_t tStart = clock();
 
+    //Allocating memory and initializing all global arrays.
     Memory_Initialization_AtStart();
     Global_Array_Initialization_AtStart();
 
+    //Reading in the structure file, and figuring out initial conditions.
     if (bReadConf == 0) {
         Initial_Conditions_Simple();
     } else if (bReadConf == 1) {
         Initial_Conditions_FromFile();
-        if (nPreSteps > 0) {
+        if (nPreSteps > 0) { //Remember that all thermalizing variants of MC moves cannot handle bonds.
             Initial_Conditions_BreakBonds();
         }
     } else {
@@ -49,6 +51,7 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+    //Performing a sanity check to see if all the beads and structures are correct.
     if (Check_System_Structure() == 0) {
         printf("Check structure sanity: OK\n");
     } else {
@@ -71,10 +74,11 @@ int main(int argc, char *argv[]) {
     printf("Thermalizing system.\n");
     printf("---------------------\n\n");
 
+    //Thermalizing the system.
     fCuTemp = fPreKT;
     Print_Data(-1, -1);//Initialization of files
     for (nGen = 0;
-         nGen < nPreSteps; nGen++) {//Intentionally not performing any data acquisition in the thermalizing phase
+         nGen < nPreSteps; nGen++) {//Intentionally not performing any data acquisition in the thermalizing phase.
         nMCInfo = MC_Step_Equil(fCuTemp);
         //printf("(%d,%d)\n", nMCInfo / 12, nMCInfo % 2);
         Print_Data(nGen, -1);
@@ -87,7 +91,7 @@ int main(int argc, char *argv[]) {
 The system has thermalized!
 */
     int run_cycle;
-
+    //Going through the MC cycles.
     for (run_cycle = 0; run_cycle < nTot_CycleNum; run_cycle++) {
         fKT = fKT_Cycle[run_cycle];//temp_cycle[run_cycle];
         Calculate_Rot_Bias(fKT_Cycle[run_cycle]);
@@ -103,6 +107,7 @@ The system has thermalized!
         Reset_Global_Arrays();
     }
 
+    //Writing everything
     Write_TotalSysProp(fileSysProp, run_cycle);
 
     tEnd = clock();
