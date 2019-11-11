d4a234f68e0d75144b5dc6c3306fd1723e23ae17
FurqanDar <darf@kenyon.edu>
2019-11-11 11:00:59 -0600

    Will only compile lassi, and not lassi_gdb

--- src/compile.sh
+++ src/compile.sh
@@ -1,3 +1,2 @@
-gcc -g -pg main.c parsekey.c print.c initialize.c structure.c energy.c cluster.c mcmove.c -o lassi_gdb -lm
 gcc -O3 main.c parsekey.c print.c initialize.c structure.c energy.c cluster.c mcmove.c -o lassi -lm
 cp  lassi ../runs/Test_Simulation/
