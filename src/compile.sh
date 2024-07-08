405d4b802b281e95475fb07dc7ef8a66d04b1f44
Jared Lalmansingh <jared.lalmansingh@wustl.edu>
2024-07-08 10:29:27 -0500

    Patches to enable compilation of @FurqanDar's verion (https://github.com/FurqanDar/LASSI_GIT) on Apple Silicon

--- src/compile.sh
+++ src/compile.sh
@@ -1,8 +1,13 @@
-gcc -march=x86-64 -O3 -std=gnu11 -lm -o lassi main.c parsekey.c print.c initialize.c structure.c energy.c cluster.c mcmove.c
+# Only uncomment if you use Intel or AMD
+#gcc -march=x86-64 
+gcc -O3 -std=gnu11 -lm -o lassi main.c parsekey.c print.c initialize.c structure.c energy.c cluster.c mcmove.c
 
-gcc -march=x86-64 -Og -ggdb -std=gnu11 -lm -o lassi_gdb main.c parsekey.c print.c initialize.c structure.c energy.c cluster.c mcmove.c
+# Only uncomment if you use Intel or AMD
+#gcc -march=x86-64 
+gcc -Og -ggdb -std=gnu11 -lm -o lassi_gdb main.c parsekey.c print.c initialize.c structure.c energy.c cluster.c mcmove.c
 
-cp -u lassi /project/fava/packages/bin/lassi
-cp -u lassi_gdb /project/fava/packages/bin/lassi_gdb
+# Specific to the Pappu Lab's computing infrastructure
+#cp -u lassi /project/fava/packages/bin/lassi
+#cp -u lassi_gdb /project/fava/packages/bin/lassi_gdb
 
-cp  lassi ../runs/Test_Simulation/
+cp lassi ../runs/Test_Simulation/
